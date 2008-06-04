b2ac8133163f6848a12ce8cdcc8eb4ae312f9aea
andre.winkler@web.de <andre.winkler@web.de@75483131-604e-0410-abed-a1f4224329e0>
2008-06-04 19:23:59 +0000

    ADD: Groovy PropertyChangeSupport.

---
+++ trunk/groovy-sandbox/src/test/groovy/de/gluehloch/sandbox/groovy/bean/GroovyPropertyChangeSupportTest.groovy
@@ -0,0 +1,11 @@
+package de.gluehloch.sandbox.groovy.bean
+
+import org.junit.Test
+import org.junit.runner.JUnitCore
+
+/**
+ * Testet die Klasse GroovyPropertyChangeSupport.
+ */
+class GroovyPropertyChangeSupportTest {
+
+}
