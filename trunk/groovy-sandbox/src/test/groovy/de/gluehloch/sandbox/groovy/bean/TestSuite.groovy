25ab13f12c6a9dce3e9b183efbfea9ab97cfbbe8
andre.winkler@web.de <andre.winkler@web.de@75483131-604e-0410-abed-a1f4224329e0>
2008-07-19 13:58:29 +0000

    UPDATE: Testerei und Ergaenzung zu PropertyChangeSupport.

---
+++ trunk/groovy-sandbox/src/test/groovy/de/gluehloch/sandbox/groovy/bean/TestSuite.groovy
@@ -0,0 +1,13 @@
+package de.gluehloch.sandbox.groovy.bean
+
+import org.junit.runner.JUnitCore
+//JUnitCore.main(PersonTest.name)
+class TestSuite {
+
+	static void main(String[] args) {
+		JUnitCore.main(
+				PersonTest.name,
+				GroovyPropertyChangeSupportTest.name)
+	}
+
+}
