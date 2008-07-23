7872af3b82500c4725408641936fd6758512015d
andre.winkler@web.de <andre.winkler@web.de@75483131-604e-0410-abed-a1f4224329e0>
2008-07-23 15:41:37 +0000

    UPDATE: Test erweitert.

--- trunk/groovy-sandbox/src/test/groovy/de/gluehloch/sandbox/groovy/bean/GroovyPropertyChangeSupportTest.groovy
+++ trunk/groovy-sandbox/src/test/groovy/de/gluehloch/sandbox/groovy/bean/GroovyPropertyChangeSupportTest.groovy
@@ -2,6 +2,7 @@ package de.gluehloch.sandbox.groovy.bean
 
 import java.beans.PropertyChangeListener
 
+import org.junit.Assert
 import org.junit.Before
 import org.junit.Test
 import org.junit.runner.JUnitCore
@@ -11,29 +12,89 @@ import org.junit.runner.JUnitCore
  */
 class GroovyPropertyChangeSupportTest {
 
-	private def callCounter
+	def gpcs
+	def expectations = []
+	def index
+
+	private void check(event) {
+		def expected = expectations[index]
+        assert expected.propertyName == event.propertyName
+        assert expected.oldValue == event.oldValue
+        assert expected.newValue == event.newValue
+	}
+
+	private void checkExpectations() {
+        expectations.eachWithIndex { value, index ->
+        gpcs.firePropertyChangeEvent(
+            expectations[index].propertyName,
+            expectations[index].oldValue,
+            expectations[index].newValue)
+        }
+	}
 
 	@Test
-	void testGroovyPropertyChangeSupport() {
+	void testGroovyPropertyChangeSupportWithAllOrNever() {
 		def listener = { event ->
-			println "NAME: ${event.propertyName}: OLD: ${event.oldValue}, NEW: ${event.newValue}"
-			callCounter++
+			check event
+			index++
 		}
 
-		def gpcs = new GroovyPropertyChangeSupport(wrappedObject: this)
+		def shouldNeverBeCalled = { event ->
+			Assert.fail()
+		}
+
+		
 		def pcl = listener as PropertyChangeListener
+
 		gpcs.addPropertyChangeListener(pcl)
-		assert gpcs.listeners.size() == 1
+		gpcs.addPropertyChangeListener(
+			'never', shouldNeverBeCalled as PropertyChangeListener)
+		assert gpcs.listeners.size() == 2
 		assert gpcs.listeners['@ALL'].size() == 1
 		assert gpcs.listeners['@ALL'][0] == pcl
+        assert gpcs.listeners['never'].size() == 1
 
-		gpcs.firePropertyChangeEvent('name', 'adam', 'lars')
-		assert callCounter == 1
+        checkExpectations()
+		assert index == 4
+	}
+
+	void testGroovyPropertyChangeSupportWithNamedListener() {
+        def listener1 = { event ->
+        	check event
+        }
+        def listener2 = { event ->
+        	check event
+        	index++
+        }
+        def listener3 = { event ->
+        	check event
+        	index++
+        }
+
+        gpcs.addPropertyChangeListener(
+        	'name', listener1 as PropertyChangeListener)
+        gpcs.addPropertyChangeListener(
+            'name', listener2 as PropertyChangeListener)
+        gpcs.addPropertyChangeListener(
+            'prop', listener3 as PropertyChangeListener)
+
+        assert gpcs.listeners.size() == 3
+        assert gpcs.listeners['name'].size() == 2
+        assert gpcs.listeners['prop'].size() == 1
+
+        checkExpectations()
+        assert index == 4
 	}
 
 	@Before
 	void setUp() {
-		callCounter = 0
+		expectations = []
+        expectations << [propertyName: 'name', odValue: 'adam', newValue: 'lars']
+        expectations << [propertyName: 'name', odValue: 'lars', newValue: 'andre']
+        expectations << [propertyName: 'name', odValue: 'andre', newValue: 'christine']
+		expectations << [propertyName: 'prop', odValue: 'wurst', newValue: 'salami']
+		index = 0
+		gpcs = new GroovyPropertyChangeSupport(wrappedObject: this)
 	}
 
 }
