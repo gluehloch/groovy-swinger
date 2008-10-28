9c86c04aca55792a2dd03ba75d59ecb6f78700a4
andre.winkler@web.de <andre.winkler@web.de@75483131-604e-0410-abed-a1f4224329e0>
2008-10-28 16:00:44 +0000

    UPDATE: Test parametrisierbar.

--- trunk/groovy-oracle/src/test/groovy/de/gluehloch/sandbox/groovy/oracle/OraUtilsTest.groovy
+++ trunk/groovy-oracle/src/test/groovy/de/gluehloch/sandbox/groovy/oracle/OraUtilsTest.groovy
@@ -9,7 +9,28 @@ class OraUtilsTest {
 
 	@Test
 	void testOraUtils() {
-		def sql = OraUtils.createSql('cb', 'cbrms', 'zora1entw.zit.commerzbank.com:1521:RE09');
+		def user = System.getProperty('groovy.oracle.test.user')
+		def pwd = System.getProperty('groovy.oracle.test.password')
+		def url = System.getProperty('groovy.oracle.test.url')
+
+		if (!user || !pwd || !url) {
+			println """Check your .settings.xml:
+  <profiles>
+    <profile>
+      <id>default</id>
+      <properties>
+        <compiler.encoding>UTF-8</compiler.encoding>
+        <groovy.oracle.test.user>user</groovy.oracle.test.user>
+        <groovy.oracle.test.password>password</groovy.oracle.test.password>
+        <groovy.oracle.test.url>host:port:sid</groovy.oracle.test.url>
+      </properties>
+    </profile>
+  </profiles>
+"""
+			throw new IllegalStateException()
+		}
+	
+		def sql = OraUtils.createSql(user, pwd, url);
 		assert sql != null
 	}
 
