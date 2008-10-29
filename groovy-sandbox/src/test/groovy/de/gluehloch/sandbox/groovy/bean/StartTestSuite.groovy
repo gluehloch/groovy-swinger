ab1c263e8f186ef01be6f29ff0cdfcb577b94ac3
andre.winkler <andre.winkler@web.de>
2008-10-29 15:12:09 +0000

    Groovy Oracle Release 0.1.0.

--- groovy-sandbox/src/test/groovy/de/gluehloch/sandbox/groovy/bean/StartTestSuite.groovy
+++
@@ -1,16 +0,0 @@
-package de.gluehloch.sandbox.groovy.bean
-
-import org.junit.runner.JUnitCore
-//JUnitCore.main(PersonTest.name)
-class StartTestSuite {
-
-	static void main(String[] args) {
-		JUnitCore.main(
-				GroovyPresentationModelTest.name,
-				GroovyPropertyChangeSupportBuilderTest.name,
-				GroovyPropertyChangeSupportTest.name,
-				JGoodiesDataBindingTest.name,
-				PersonTest.name)
-	}
-
-}
