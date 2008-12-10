b0c19148416cd1e238a5b876bfa069eb540a5ebf
andre.winkler@web.de <andre.winkler@web.de@75483131-604e-0410-abed-a1f4224329e0>
2008-12-10 14:21:34 +0000

    Under Construction...

--- trunk/groovy-example/src/main/groovy/de/gluehloch/sandbox/groovy/example/ChainingExample.groovy
+++ trunk/groovy-example/src/main/groovy/de/gluehloch/sandbox/groovy/example/ChainingExample.groovy
@@ -47,17 +47,46 @@ class ChainingExample{
 
     def chain(List commands) {
         def chainToCall = {}
-        commands.reverse().each {command->
+        commands.reverse().each {command ->
             chainToCall = command.curry(chainToCall)
         }
         chainToCall()
     }
 
-    def example_1() {
+    def closureChain() {
     	assert 'three*two*one* Andre *one*two*three' == three {two {one({' Andre '})}}
 
     	def myChain = three.curry(two.curry(one.curry({' Andre '})))
     	assert 'three*two*one* Andre *one*two*three' == myChain()
+
+    	println chain([one,two,three])
+    	// TODO: Die folgende Bedingung kann von der Methode #chain nicht
+    	// erfüllt werden.
+        //assert 'three*two*one* null *one*two*three' == chain([one,two,three])
+    }
+
+    def curryExample() {
+        def concat = { x, y ->
+            x + delegate.call() + y
+        }
+        concat.delegate = { " " }
+        assert concat("Hello", "World") == "Hello World"
+
+        // Chained Closure (Something like a curry).
+        def hello = { x -> concat("Hello", x) }
+        assert hello("World") == "Hello World"
+
+        // The same processing but with curry.
+        hello = concat.curry("Hello")
+        assert hello("World") == "Hello World"
+
+        hello = concat.curry("Hello")
+        hello.delegate = { "," }
+        assert hello("World") == "Hello,World"
+
+        hello = { x -> concat("Hello", x) }
+        hello.delegate = { "," }
+        assert hello("World") == "Hello World"
     }
 
 }
