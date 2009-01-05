48bb197a7bf5f0da9d9715216e733ade0994757c
andre.winkler@web.de <andre.winkler@web.de@75483131-604e-0410-abed-a1f4224329e0>
2009-01-05 13:30:55 +0000

    Under construction...

--- trunk/oracle-unit-test/src/packages/UT.sql
+++ trunk/oracle-unit-test/src/packages/UT.sql
@@ -1,50 +1,44 @@
 CREATE OR REPLACE
-PACKAGE TEST AS
+PACKAGE UT AS
 
   ASSERTION_FAILED EXCEPTION;
   PRAGMA EXCEPTION_INIT(ASSERTION_FAILED, -20100);
 
-  FUNCTION assert_equals
+  PROCEDURE assert_equals
   (
      p_expected BOOLEAN
     ,p_value    BOOLEAN
-  )
-  RETURN BOOLEAN;
+  );
 
-  FUNCTION assert_equals
+  PROCEDURE assert_equals
   (
      p_expected DATE
     ,p_value    DATE
-  )
-  RETURN BOOLEAN;
+  );
 
-  FUNCTION assert_equals
+  PROCEDURE assert_equals
   (
      p_expected CHAR
     ,p_value    CHAR
-  )
-  RETURN BOOLEAN;
+  );
 
-  FUNCTION assert_equals
+  PROCEDURE assert_equals
   (
      p_expected VARCHAR2
     ,p_value    VARCHAR2
-  )
-  RETURN BOOLEAN;
+  );
 
-  FUNCTION assert_equals
+  PROCEDURE assert_equals
   (
      p_expected NUMBER
     ,p_value    NUMBER
-  )
-  RETURN BOOLEAN;
+  );
 
-  FUNCTION assert_equals
+  PROCEDURE assert_equals
   (
      p_expected PLS_INTEGER
     ,p_value    PLS_INTEGER
-  )
-  RETURN BOOLEAN;
+  );
 
-END TEST;
+END UT;
 /
