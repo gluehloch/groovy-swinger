d9dc39825cd9e2bb1ca2f4f5a134cfdad50ac0d7
andre.winkler@web.de <andre.winkler@web.de@75483131-604e-0410-abed-a1f4224329e0>
2009-02-03 16:54:35 +0000

    ADD: Example for the keyword delegate.

---
+++ trunk/groovy-example/src/script/DelegateExample.groovy
@@ -0,0 +1,12 @@
+def clos = {
+    println 'this: ' + this.class.name
+    println 'delegate: ' + delegate.class.name
+    return delegate.class.name
+}
+
+assert 'DelegateExample' == clos()
+clos.delegate = new Integer(100)
+assert 'java.lang.Integer' == clos()
+
+def ex = new Example()
+assert 'Example' == ex.doSomething()
