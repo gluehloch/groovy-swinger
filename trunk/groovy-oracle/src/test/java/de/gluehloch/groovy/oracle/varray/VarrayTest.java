40c40e42a71026d735faacdde95033a77ccbbdbc
andre.winkler@web.de <andre.winkler@web.de@75483131-604e-0410-abed-a1f4224329e0>
2009-02-14 00:40:19 +0000

    Test for ORACLE varrays.

---
+++ trunk/groovy-oracle/src/test/java/de/gluehloch/groovy/oracle/varray/VarrayTest.java
@@ -0,0 +1,147 @@
+/*
+ * $Id: VarrayTest.java 87 2009-02-10 19:57:53Z andre.winkler@web.de $
+ * ============================================================================
+ * Project groovy-oracle
+ * Copyright (c) 2008-2009 by Andre Winkler. All rights reserved.
+ * ============================================================================
+ *          GNU LESSER GENERAL PUBLIC LICENSE
+ *  TERMS AND CONDITIONS FOR COPYING, DISTRIBUTION AND MODIFICATION
+ *
+ *  This library is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public
+ *  License as published by the Free Software Foundation; either
+ *  version 2.1 of the License, or (at your option) any later version.
+ *
+ *  This library is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public
+ *  License along with this library; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
+ */
+
+package de.gluehloch.groovy.oracle.varray;
+
+import java.sql.CallableStatement;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.Statement;
+
+import oracle.jdbc.oracore.OracleTypeCOLLECTION;
+import oracle.sql.ARRAY;
+import oracle.sql.ArrayDescriptor;
+import oracle.sql.SQLName;
+
+import org.apache.commons.lang.StringUtils;
+import static org.junit.Assert.*;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+/**
+ * Testet den Umgang mit Oracles VARRAYs.
+ * 
+ * @author  $Author: andrewinkler $
+ * @version $Revision: 1472 $ $Date: 2008-10-21 15:24:34 +0200 (Di, 21 Okt 2008) $
+ */
+public class VarrayTest {
+
+	private static final String ORACLE_URL = "jdbc:oracle:thin:${user}/${password}@${url}";
+
+	private static Connection conn;
+
+	@Test
+	public void testOraclesVarray() throws Exception {
+		CallableStatement cs = null;
+		try {
+			String arrayElements[] = { "Test3", "Test4", "Test5" };
+			ArrayDescriptor desc = ArrayDescriptor.createDescriptor(
+				"T_STRING_VARRAY", conn);
+			ARRAY newArray = new ARRAY(desc, conn, arrayElements);
+
+			String spCall = "{ call call_me(?, ?) }";
+			cs = conn.prepareCall(spCall);
+			cs.setArray(1, newArray);
+			cs.registerOutParameter(2, java.sql.Types.INTEGER);
+
+			cs.execute();
+			assertEquals(3, cs.getInt(2));
+		} finally {
+			if (cs != null) {
+				cs.close();
+			}
+		}
+
+	}
+
+	@Test
+	public void testOraclesVarrayWithPackage() throws Exception {
+		CallableStatement cs = null;
+		try {
+			String arrayElements[] = { "Test3", "Test4", "Test5" };
+//			int n = OracleTypeCOLLECTION.TYPE_PLSQL_INDEX_TABLE;
+//			ArrayDescriptor desc = ArrayDescriptor.createDescriptor();
+			ArrayDescriptor desc = ArrayDescriptor.createDescriptor(
+				"CP_TEST.t_ROWNUMBER", conn);
+			ARRAY newArray = new ARRAY(desc, conn, arrayElements);
+
+			String spCall = "{ call CP_TEST.call_me(?, ?) }";
+			cs = conn.prepareCall(spCall);
+			cs.setArray(1, newArray);
+			cs.registerOutParameter(2, java.sql.Types.INTEGER);
+
+			cs.execute();
+			assertEquals(3, cs.getInt(2));
+		} finally {
+			if (cs != null) {
+				cs.close();
+			}
+		}
+
+	}
+
+	@BeforeClass
+	public static void setUpClass() throws Exception {
+		String[] tokens = new String[] { "${user}", "${pwd}", "${url}" };
+
+		String user = System.getProperty("groovy.oracle.test.user");
+		String pwd = System.getProperty("groovy.oracle.test.password");
+		String url = System.getProperty("groovy.oracle.test.url");
+
+		String[] replacements = new String[] { user, pwd, url };
+
+		DriverManager.registerDriver(new oracle.jdbc.OracleDriver());
+		String oracleUrl = StringUtils.replaceEach(ORACLE_URL, tokens,
+			replacements);
+
+		conn = DriverManager.getConnection(oracleUrl, user, pwd);
+		conn.setAutoCommit(false);
+
+		Statement stmt = conn.createStatement();
+		stmt
+			.execute("CREATE TYPE t_string_varray AS VARRAY(10) OF VARCHAR2(100)");
+		stmt
+			.execute("CREATE PROCEDURE call_me(p_strings IN t_string_varray, p_retcode OUT NUMBER) IS BEGIN p_retcode := p_strings.COUNT; END call_me;");
+		stmt.execute("ALTER PROCEDURE call_me COMPILE");
+	}
+
+	@AfterClass
+	public static void tearDownClass() throws Exception {
+		try {
+			if (conn != null) {
+				Statement stmt = conn.createStatement();
+				stmt.execute("DROP PROCEDURE call_me");
+				stmt.execute("DROP TYPE t_string_varray");
+			}
+		} finally {
+			if (conn != null) {
+				conn.close();
+				conn = null;
+			}
+		}
+	}
+
+}
