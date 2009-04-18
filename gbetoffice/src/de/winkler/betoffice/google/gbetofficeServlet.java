094163d1f464e46684566b073bdb98ce835dc736
andre.winkler <andre.winkler@web.de>
2009-04-18 13:16:45 +0000

    ADD: Output of a user request for a Bundesliga match.

--- gbetoffice/src/de/winkler/betoffice/google/gbetofficeServlet.java
+++ gbetoffice/src/de/winkler/betoffice/google/gbetofficeServlet.java
@@ -5,21 +5,90 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.List;
 
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.dom4j.Document;
+import org.dom4j.DocumentException;
+import org.dom4j.DocumentHelper;
+import org.dom4j.Element;
+
 @SuppressWarnings("serial")
 public class gbetofficeServlet extends HttpServlet {
+
+	public void doPost(HttpServletRequest req, HttpServletResponse resp)
+		throws IOException {
+		resp.setContentType("text/html");
+
+		String year = req.getParameter("year").toString();
+		String round = req.getParameter("round").toString();
+
+		resp.getWriter().println("<html><body>");
+		resp.getWriter().println(
+			"Searching for : " + year + ", " + round + "<br/>");
+
+		// http://www.dfb.de/bliga/bundes/archiv/2008/xml/blm_e_23_08.xml
+
+		StringBuffer sb = new StringBuffer(
+			"http://www.dfb.de/bliga/bundes/archiv/");
+		sb.append(year).append("/xml/blm_e_").append(round).append("_").append(
+			year.substring(2, 4)).append(".xml");
+		resp.getWriter().println(sb.toString() + "<br/><br/>");
+		try {
+			URL url = new URL(sb.toString());
+			BufferedReader reader = new BufferedReader(new InputStreamReader(
+				url.openStream()));
+
+			StringBuffer xml = new StringBuffer();
+			String line;
+			while ((line = reader.readLine()) != null) {
+				xml.append(line);
+			}
+			Document document = DocumentHelper.parseText(xml.toString());
+			List termine = document.selectNodes("//ergx/termin");
+			int index = 1;
+			for (Object termin : termine) {
+				Element terminNode = (Element) termin;
+				resp.getWriter().println("Termin " + index + " : "+ terminNode.element("datum").getText() + "<br/>");
+				resp.getWriter().println("Heim:" + terminNode.element("teama").getText() + "<br/>");
+				resp.getWriter().println("Gast:" + terminNode.element("teamb").getText() + "<br/>");
+				resp.getWriter().println("Ergebnis:" + terminNode.element("punkte_a").getText() + ":" + terminNode.element("punkte_b").getText() + "<br/>");
+				resp.getWriter().println("<br/>");
+				index++;
+			}
+
+			resp.getWriter().println();
+			resp.getWriter().println("</body></html>");
+			reader.close();
+
+		} catch (MalformedURLException ex) {
+			throw new RuntimeException(ex);
+		} catch (IOException ex) {
+			throw new RuntimeException(ex);
+		} catch (DocumentException ex) {
+			throw new RuntimeException(ex);
+		}
+	}
 
 	public void doGet(HttpServletRequest req, HttpServletResponse resp)
 		throws IOException {
 
 		resp.setContentType("text/plain");
+
+		String year = req.getAttribute("year").toString();
+		String round = req.getAttribute("round").toString();
+
+		resp.getWriter().println("<html><body>");
+		resp.getWriter().println(
+			"Searching for : " + year + ", " + round + "<br/>");
+
 		StringBuffer sb = new StringBuffer();
 		try {
-			URL url = new URL("http://www.dfb.de/bliga/bundes/archiv/2008/xml/blm_e_23_08.xml");
+			URL url = new URL(
+				"http://www.dfb.de/bliga/bundes/archiv/2008/xml/blm_e_23_08.xml");
 			BufferedReader reader = new BufferedReader(new InputStreamReader(
 				url.openStream()));
 
@@ -27,15 +96,15 @@ public class gbetofficeServlet extends HttpServlet {
 			while ((line = reader.readLine()) != null) {
 				resp.getWriter().println(line);
 			}
+			resp.getWriter().println();
+			resp.getWriter().println("</body></html>");
 			reader.close();
 
-		} catch (MalformedURLException e) {
-			// ...
-		} catch (IOException e) {
-			// ...
+		} catch (MalformedURLException ex) {
+			throw new RuntimeException(ex);
+		} catch (IOException ex) {
+			throw new RuntimeException(ex);
 		}
-
-		resp.getWriter().println("Hello, world");
 	}
 }
 
