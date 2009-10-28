083e0e8005f3f6c83f9c1802b06b85cd2f8a7224
andre.winkler@web.de <andre.winkler@web.de@75483131-604e-0410-abed-a1f4224329e0>
2009-10-28 15:28:37 +0000

    UPDATE: SVN keywords; Maven update; Eclipse 3.5; ...

--- trunk/groovy-sandbox/src/test/groovy/de/gluehloch/sandbox/groovy/bean/JGoodiesDataBindingTest.groovy
+++ trunk/groovy-sandbox/src/test/groovy/de/gluehloch/sandbox/groovy/bean/JGoodiesDataBindingTest.groovy
@@ -1,10 +1,34 @@
+/*
+ * $Id$
+ * ============================================================================
+ * Project groovy-swinger
+ * Copyright (c) 2008-2009 by Andre Winkler. All rights reserved.
+ * ============================================================================
+ *          GNU LESSER GENERAL PUBLIC LICENSE
+ *  TERMS AND CONDITIONS FOR COPYING, DISTRIBUTION AND MODIFICATION
+ *
+ *  This library is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public
+ *  License as published by the Free Software Foundation; either
+ *  version 2.1 of the License, or (at your option) any later version.
+ *
+ *  This library is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public
+ *  License along with this library; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
+ */
+
 package de.gluehloch.sandbox.groovy.bean
 
 import java.beans.PropertyChangeListener
 
 import org.junit.Test
 
-import java.io.*
 import javax.swing.*
 
 import com.jgoodies.binding.*
@@ -13,6 +37,9 @@ import com.jgoodies.binding.value.*
 /**
  * Testet das Binding Framework von JGoodies und unseren
  * PropertyChangeSupportBuilder.
+ *
+ * @author  $Author$
+ * @version $Revision$ $Date$
  */
 class JGoodiesDataBindingTest {
 
@@ -25,17 +52,22 @@ class JGoodiesDataBindingTest {
         pb = GroovyPropertyChangeSupportBuilder.preparePCLMechanics(pb)
 
         def valueHolders = [:]
-        pb.metaClass.properties.each { metaProperty ->
+        def properties = ['name', 'surname', 'age', 'gender']
+        //pb.metaClass.properties.each { metaProperty ->
+        properties.each { metaProperty ->
         	ValueHolder vh = new ValueHolder()
-        	valueHolders[metaProperty.name] = vh
+        	//valueHolders[metaProperty.name] = vh
+            valueHolders[metaProperty] = vh
 
         	def listener = { event ->
         		vh.value = event.newValue
         	} as PropertyChangeListener
-        	pb.addPropertyChangeListener(metaProperty.name, listener)
+        	//pb.addPropertyChangeListener(metaProperty.name, listener)
+            pb.addPropertyChangeListener(metaProperty, listener)
 
             def valueListener = { event ->
-                pb.@"${metaProperty.name}" = event.newValue
+                // pb.@"${metaProperty.name}" = event.newValue
+                pb.@"${metaProperty}" = event.newValue
             } as PropertyChangeListener
             vh.addValueChangeListener(valueListener)
         }
