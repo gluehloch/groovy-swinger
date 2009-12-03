fb49876bb28a3175dbd0add2f04b341c014d35ff
andre.winkler <andre.winkler@web.de>
2009-12-03 20:52:45 +0000

    ADD: Example for a SelectionInListModel and a JCombobox.

---
+++ groovy-sandbox/src/test/groovy/de/gluehloch/sandbox/groovy/bean/SelectionInListModel.groovy
@@ -0,0 +1,43 @@
+/*
+ * $Id: SelectionInListModel.java 157 2009-11-12 07:42:42Z andre.winkler@web.de $
+ * ============================================================================
+ * Project groovy-swinger
+ * Copyright (c) 2008-2009 by Andre Winkler. All rights reserved.
+ * ============================================================================
+ *          GNU LESSER GENERAL PUBLIC LICENSE
+ *  TERMS AND CONDITIONS FOR COPYING, DISTRIBUTION AND MODIFICATION
+ *
+ *  This library is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public
+ *  License as published by the Free Software Foundation; either
+ *  version 2.1 of the License, or (at your option) any later version.
+ *
+ *  This library is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public
+ *  License along with this library; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
+ */
+
+package de.gluehloch.sandbox.groovy.bean;
+
+import ca.odell.glazedlists.BasicEventList;
+import ca.odell.glazedlists.EventList;
+
+/**
+ * Ein Groovy GUI Modell für <code>SelectionInListTest</code>.
+ *
+ * @author  $Author: andre.winkler@web.de $
+ * @version $Revision: 157 $ $Date: 2009-11-12 08:42:42 +0100 (Do, 12 Nov 2009) $
+ */
+class SelectionInListModel {
+
+    String selectedItem
+
+    final EventList<String> selectionList = new BasicEventList<String>();
+
+}
---
+++ groovy-sandbox/src/test/java/de/gluehloch/sandbox/groovy/bean/SelectionInListTest.java
@@ -0,0 +1,58 @@
+/*
+ * $Id: PresentationModelTest.java 157 2009-11-12 07:42:42Z andre.winkler@web.de $
+ * ============================================================================
+ * Project groovy-swinger
+ * Copyright (c) 2008-2009 by Andre Winkler. All rights reserved.
+ * ============================================================================
+ *          GNU LESSER GENERAL PUBLIC LICENSE
+ *  TERMS AND CONDITIONS FOR COPYING, DISTRIBUTION AND MODIFICATION
+ *
+ *  This library is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public
+ *  License as published by the Free Software Foundation; either
+ *  version 2.1 of the License, or (at your option) any later version.
+ *
+ *  This library is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public
+ *  License along with this library; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
+ */
+
+package de.gluehloch.sandbox.groovy.bean;
+
+import javax.swing.JComboBox;
+
+import org.junit.Test;
+
+import ca.odell.glazedlists.swing.EventListModel;
+
+import com.jgoodies.binding.adapter.ComboBoxAdapter;
+import com.jgoodies.binding.list.SelectionInList;
+
+/**
+ * Testet die JGoodies Klasse {@link SelectionInList} im Zusammenspiel mit
+ * {@link GroovyPresentationModel}.
+ *
+ * @author  $Author: andre.winkler@web.de $
+ * @version $Revision: 157 $ $Date: 2009-11-12 08:42:42 +0100 (Do, 12 Nov 2009) $
+ */
+public class SelectionInListTest {
+
+    @SuppressWarnings("unchecked")
+    @Test
+    public void testSelectionInListWithGroovyPresentationModel() {
+        final String[] fruits = new String[] { "apfel", "birne", "pflaume"};
+        SelectionInListModel silm = new SelectionInListModel();
+        GroovyPresentationModel gpm = new GroovyPresentationModel(silm);
+
+        EventListModel elm = new EventListModel(silm.getSelectionList());
+        SelectionInList sil = new SelectionInList(elm, gpm.getModel("selectedItem"));
+        JComboBox combobox = new JComboBox(new ComboBoxAdapter(sil));
+    }
+
+}
