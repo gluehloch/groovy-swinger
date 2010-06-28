713af5cb3c5308d4962471dc89c2c2e5efef2fca
andre.winkler <andre.winkler@web.de>
2010-06-28 14:34:20 +0000

    ADD: Textfield builder

---
+++ groovy-sandbox/src/test/java/de/awtools/groovy/swinger/builder/TextfieldTest.java
@@ -0,0 +1,44 @@
+/*
+ * $Id: Textfield.java 1968 2010-01-21 20:26:23Z andrewinkler $
+ * ============================================================================
+ * Project groovy-swinger
+ * Copyright (c) 2008-2010 by Andre Winkler. All rights reserved.
+ * ============================================================================
+ *          GNU LESSER GENERAL PUBLIC LICENSE
+ *  TERMS AND CONDITIONS FOR COPYING, DISTRIBUTION AND MODIFICATION
+ *
+ *  This library is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public
+ *  License as published by the Free Software Foundation; either
+ *  version 2.1 of the License, or (at your option) any later version.
+ *
+ *  This library is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public
+ *  License along with this library; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
+ */
+
+package de.awtools.groovy.swinger.builder;
+
+import org.junit.Test;
+
+/**
+ * TODO: Test of class {@link Textfield}.
+ *
+ * @author by Andre Winkler, $LastChangedBy: andrewinkler $
+ * @version $LastChangedRevision: 1968 $ $LastChangedDate: 2010-01-21 21:26:23 +0100 (Do, 21 Jan 2010) $
+ */
+public class TextfieldTest {
+
+    @Test
+    public void testTextfield() {
+        Textfield textfield = Textfield.TextfieldBuilder.textfield("name")
+            .columns(20).mandatory().editable();
+    }
+
+}
