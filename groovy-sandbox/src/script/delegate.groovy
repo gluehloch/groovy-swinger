22cc0b186326e0d4cf8f15215a8ab8e713c607ca
andre.winkler <andre.winkler@web.de>
2010-06-29 16:29:52 +0000

    ADD: Some trials...

---
+++ groovy-sandbox/src/script/delegate.groovy
@@ -0,0 +1,42 @@
+class Example {
+    def func = {
+        second()
+    }
+    
+    def second = {
+        "second value"
+    }
+
+    def invokeMethod(String name, args) {
+        println "invokeMethod ${name}"
+        super.invokeMethod(name, args)
+    }
+
+    def propertyMissing(String name) {
+        println "missingProperty ${name}"
+        "missingProperty"
+    }
+
+    def propertyMissing(String name, value) {
+        println "missingProperty ${name}"
+    }
+}
+
+class AnotherClass {
+    def second = {
+        "another value"
+    }
+}
+
+println "Hello Andre"
+def example = new Example()
+
+println "1: ${example.func()}"
+example.func.delegate = new AnotherClass()
+println "2: ${example.func()}"
+
+example.func.resolveStrategy = Closure.DELEGATE_FIRST
+println "3: ${example.func()}"
+
+println "4: Invoke missing method: ${example.stichmon}"
+
